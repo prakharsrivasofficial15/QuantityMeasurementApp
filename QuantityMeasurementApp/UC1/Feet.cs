d9635251fc922e96330096887cd5486300a03f72
prakharsrivasofficial15 <prakhar.official15@gmail.com>
2026-02-22 11:53:04 +0530

    Add. UC1: Implemented Feet equality feature with NUnit test cases

---
+++ QuantityMeasurementApp/UC1/Feet.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace QuantityMeasurementApp.UC1
+{
+    public class Feet
+    {
+        public double Value { get; }
+
+        public Feet(double value)
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj is null || obj.GetType() != typeof(Feet))
+                return false;
+
+            var other = (Feet)obj;
+
+            return Value.CompareTo(other.Value) == 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
